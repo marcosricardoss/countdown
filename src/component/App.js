fefefd7d5404e7a703470c92774e5c8deddf5666
Marcos Ricardo <marcosricardoss@gmail.com>
2019-07-01 10:24:21 -0300

    feat: add default props values to the Countdown component.

--- src/component/App.js
+++ src/component/App.js
@@ -2,10 +2,15 @@ import React from 'react';
 import Countdown from "./Countdown";
 import './App.css';
 
+let nextYear = new Date(new Date(0, 0, 1).setFullYear(new Date().getFullYear() + 1))
+Countdown.defaultProps = {
+  'dateTo': { year: nextYear.getFullYear(), month: nextYear.getMonth(), date: nextYear.getDay() }
+}
+
 function App() {
   return (
     <div className="component-app">
-      <Countdown dateTo={{ year: 2019, month: 6, date: 2 }}/>
+      <Countdown />
     </div>
   );
 }
