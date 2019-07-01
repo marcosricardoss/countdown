9a43449946b419c26ba892ae4fd0d04d2599433d
Marcos Ricardo <marcosricardoss@gmail.com>
2019-07-01 16:54:07 -0300

    refactor: lift the Countdown component state up to the App component.

--- src/component/App.js
+++ src/component/App.js
@@ -6,23 +6,44 @@ import "./App.css";
 export default class App extends React.Component {
   constructor(props){
     super(props)
-    this.state = {     
-      dateTo: new Date(new Date(0, 0, 1).setFullYear(new Date().getFullYear() + 1))
+    this.state = {    
+      now: new Date(), 
+      timeTo: new Date(new Date(0, 0, 1).setFullYear(new Date().getFullYear() + 1))
     }
     this.handleChange.bind(this);
   }
 
-  handleChange(date) {
-    this.setState({    
-      dateTo: date
+
+  componentDidMount() {
+    this.timerID = setInterval(
+        () => this.timer(),
+        1000
+    );
+  }
+
+  componentWillUnmount() {
+      clearInterval(this.timerID);
+  }
+
+  timer() {    
+    this.setState({       
+      now: new Date()
     });
+  }
+
+  handleChange(time) {
+    if (time > new Date()) {
+      this.setState({
+        timeTo: time
+      });
+    }    
   }
 
   render() {
     return (
       <div className="component-app">
-        <Countdown dateTo={this.state.dateTo} />
-        <ButtonPanel date={this.state.dateTo} onChange={(i) => this.handleChange(i)} />
+        <Countdown now={this.state.now} timeTo={this.state.timeTo} />
+        <ButtonPanel date={this.state.timeTo} onChange={(i) => this.handleChange(i)} />
       </div>
     );
   }  
--- src/component/Countdown.js
+++ src/component/Countdown.js
@@ -5,41 +5,16 @@ import pluralize from "../utils/pluralize";
 import "./Countdown.css";
 
 export default class Countdown extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-           dateTo: moment(this.props.dateTo),
-           day: 0, hour: 0, min: 0, sec: 0
-        };   
-    }
-
-    componentDidMount() {
-        this.timerID = setInterval(
-            () => this.timer(),
-            1000
-        );
-    }
-
-    componentWillUnmount() {
-        clearInterval(this.timerID);
-    }
-
-    timer() {
-        let now = moment();
-        let sec = this.state.dateTo.diff(now, "seconds") % 60;
-        let min = this.state.dateTo.diff(now, "minutes") % 60;
-        let hour = this.state.dateTo.diff(now, "hours") % 24;
-        let day = this.state.dateTo.diff(now, "days");
-        this.setState({
-            dateTo: moment(this.props.dateTo),
-            day: pad(day, 2), hour: pad(hour, 2), min: pad(min, 2), sec: pad(sec, 2)
-        });
-    }
-
     render() {
+        let now = moment(this.props.now)
+        let timeTo = moment(this.props.timeTo)
+        let sec = timeTo.diff(now, "seconds") % 60;
+        let min = timeTo.diff(now, "minutes") % 60;
+        let hour = timeTo.diff(now, "hours") % 24;
+        let day = timeTo.diff(now, "days");
         return (
             <div>          
-                { pluralize(this.state.day, 'day') } { this.state.hour }:{ this.state.min }:{ this.state.sec }
+                { pluralize(day, 'day') } { pad(hour, 2) }:{ pad(min, 2) }:{ pad(sec, 2) }
             </div>
         );
     }
