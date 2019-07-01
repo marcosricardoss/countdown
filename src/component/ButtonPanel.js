1c5771c99937c235f936a3da43e287153f17d6bc
Marcos Ricardo <marcosricardoss@gmail.com>
2019-07-01 16:57:13 -0300

    feat: add a custom input to the DatePicker of the ButtonPanel component.

--- src/component/ButtonPanel.js
+++ src/component/ButtonPanel.js
@@ -1,15 +1,36 @@
 import React from "react";
+import PropTypes from 'prop-types';
 import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
 import "./ButtonPanel.css";
 
+class ButtonInput extends React.Component {
+    render () {
+        return (
+        <button
+            className="example-custom-input"
+            onClick={this.props.onClick}>
+            {this.props.value}
+        </button>
+        )
+    }
+}
+
+ButtonInput.propTypes = {
+    onClick: PropTypes.func,
+    value: PropTypes.string
+};
+
 export default class ButtonPanel extends React.Component {
     render() {
         return (
-            <div>
+            <div>                
                 <DatePicker 
+                    customInput={<ButtonInput />}
                     selected={this.props.date}
                     onChange={this.props.onChange}
+                    showTimeSelect
+                    timeFormat="HH:mm"
                 />
             </div>
         );
