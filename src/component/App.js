e29036d7f5c8cf71c015a6fcc60adc46e230e1e1
Marcos Ricardo <marcosricardoss@gmail.com>
2019-07-01 11:36:42 -0300

    refactor: turn the App function component to a class component.
    In addition, adds an internal state to deal with OnChange event requests coming from the ButtonPanel component.

--- src/component/App.js
+++ src/component/App.js
@@ -1,18 +1,29 @@
-import React from 'react';
+import React from "react";
 import Countdown from "./Countdown";
-import './App.css';
+import ButtonPanel from "./ButtonPanel"
+import "./App.css";
 
-let nextYear = new Date(new Date(0, 0, 1).setFullYear(new Date().getFullYear() + 1))
-Countdown.defaultProps = {
-  'dateTo': { year: nextYear.getFullYear(), month: nextYear.getMonth(), date: nextYear.getDay() }
+export default class App extends React.Component {
+  constructor(props){
+    super(props)
+    this.state = {     
+      dateTo: new Date(new Date(0, 0, 1).setFullYear(new Date().getFullYear() + 1))
+    }
+    this.handleChange.bind(this);
+  }
+
+  handleChange(date) {
+    this.setState({    
+      dateTo: date
+    });
+  }
+
+  render() {
+    return (
+      <div className="component-app">
+        <Countdown dateTo={this.state.dateTo} />
+        <ButtonPanel date={this.state.dateTo} onChange={(i) => this.handleChange(i)} />
+      </div>
+    );
+  }  
 }
-
-function App() {
-  return (
-    <div className="component-app">
-      <Countdown />
-    </div>
-  );
-}
-
-export default App;
