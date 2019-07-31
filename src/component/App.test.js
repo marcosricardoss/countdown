54cff5fc147ba89cfc980d5f0849242fc0b40c29
Marcos Ricardo <marcosricardoss@gmail.com>
2019-07-31 17:28:12 -0300

    test: update App.test.js

--- src/component/App.test.js
+++ src/component/App.test.js
@@ -1,9 +1,27 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import { shallow } from 'enzyme';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
-});
+import App from './App';
+import Countdown from './Countdown';
+import Sidebar from './Sidebar';
+
+describe('App', () => {
+  describe('Rendering', () => {    
+
+    it('should have a App component', () => {
+      const wrapper = shallow(<App />);
+      expect(wrapper)
+    });
+
+    it('should have a Countdown component', () => {
+      const wrapper = shallow(<App />);
+      expect(wrapper.find(Countdown).length).toBe(1);
+    });
+
+    it('should have a Sidebar componentn', () => {
+      const wrapper = shallow(<Sidebar />);
+      expect(wrapper.find(Sidebar).length).toBe(0);
+    });
+
+  }); 
+});    
